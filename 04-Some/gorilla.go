126627c74105ea82a87636c15ca9eb843e21018a
zhaojiacheng <zhaomo0823@gmail.com>
2022-11-07 11:11:22 +0800

    官方db Demo及mod和下载依赖

---
+++ 04-Some/gorilla.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	// url  http://localhost/books/Test/page/10
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	})
+
+	http.ListenAndServe(":80", r)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module 04-Some
+
+go 1.19
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+)
